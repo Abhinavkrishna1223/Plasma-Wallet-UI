c5274cbb3f3e61de846c434e2599533f4022207e
Abhinav Krishna <123726072+AbhinvKrishna@users.noreply.github.com>
2023-06-15 01:47:03 -0700

    lil update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './Header.css';
 
-import phonoImage from '../../assets/header-imgs/download 1.png'
+//import phonoImage from '../../assets/header-imgs/download 1.png'
+import wallImg from '../../assets/header-imgs/Group 38.png'
+import wallet from '../../assets/header-imgs/Group 39.png'
 
 
 function Header() {
@@ -10,8 +12,18 @@ function Header() {
       
     <div className="ph-container">
 
+      
+
       <div className="main-img">
-        <img src={phonoImage} alt="" />
+
+      <div className="wall">
+        <img src={wallImg} alt="" />
+      </div>
+
+        <div className="wallet-img">
+        <img src={wallet} alt="" />
+
+        </div>
       </div>
 
     </div>
